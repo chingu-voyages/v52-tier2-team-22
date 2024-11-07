50ed182789a9afa1343505067957701a349cd602
predrag jandric <predrag.jandric.bg@gmail.com>
2024-11-07 23:33:52 +0100

    will ADD: initial empty project template created with vite + react + vanillaJS + installed and configured TailwindCSS

---
+++ solar-panel-planner/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen w-full bg-slate-700 place-content-center">
+      <h1 className="font-bold text-center text-5xl underline text-teal-400">
+        hello react
+      </h1>
+    </div>
+  );
+}
+
+export default App;
