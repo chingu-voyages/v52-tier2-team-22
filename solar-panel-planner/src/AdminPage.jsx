1b4de6d62a75dd755e14cdd7729076e36a662265
predrag jandric <predrag.jandric.bg@gmail.com>
2024-11-09 23:12:21 +0100

    will ADD: redux toolkit npm package and will configure store.js and add initial slice

---
+++ solar-panel-planner/src/AdminPage.jsx
@@ -0,0 +1,9 @@
+function AdminPage() {
+  return (
+    <section className="h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-5xl text-green-700">Welcome, Admin!</h1>
+    </section>
+  );
+}
+
+export default AdminPage;
