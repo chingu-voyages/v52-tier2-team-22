12e6889c41ce669cb431fd0f5a2dcc3c35bf84fe
Insomniac <ikrammaizi1@gmail.com>
2024-11-16 19:15:11 +0100

    will ADD: 3 responsive feature sections to the landing page

--- solar-panel-planner/src/pages/Homepage.jsx
+++ solar-panel-planner/src/pages/Homepage.jsx
@@ -4,6 +4,9 @@ import ResidentForm from "../ResidentForm";
 import AdminForm from "../AdminForm";
 import landingpage from "../assets/landingpage.png";
 import Navbar from "../components/Navbar";
+import Img1 from "../assets/img1.jpg"
+import Img2 from "../assets/img2.webp"
+import Img3 from "../assets/img3.jpeg"
 
 function Homepage() {
   const [selectedForm, setSelectedForm] = useState(null);
@@ -18,7 +21,47 @@ function Homepage() {
       {selectedForm === "admin" && <AdminForm />} */}
 
 
-      
+      <div className="flex flex-col gap-12 px-6 py-12 bg-background">
+      <div className="flex flex-col-reverse md:flex-row items-center md:justify-between gap-6">
+        <div className="text-center md:text-left max-w-md">
+          <h2 className="text-2xl font-bold mb-4">Send a Request</h2>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <img
+          src={Img1}
+          alt="Send a Request"
+          className="w-full md:w-1/2 rounded-lg shadow-lg"
+        />
+      </div>
+      <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+        <img
+          src={Img2}
+          alt="Evaluation"
+          className="w-full md:w-1/2 rounded-lg shadow-lg"
+        />
+        <div className="text-center md:text-left max-w-md">
+          <h2 className="text-2xl font-bold mb-4">Evaluation</h2>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col-reverse md:flex-row items-center md:justify-between gap-6 ">
+        <div className="text-center md:text-left max-w-md">
+          <h2 className="text-2xl font-bold mb-4">Installation</h2>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <img
+          src={Img3}
+          alt="Installation"
+          className="w-full md:w-1/2 rounded-lg shadow-lg"
+        />
+      </div>
+    </div>
     </main>
   );
 }
