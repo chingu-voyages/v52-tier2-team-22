1a5f575c8017896549f13f3708a6a05f4545f4cf
Insomniac <ikrammaizi1@gmail.com>
2024-11-23 20:54:21 +0100

    will ADD the form to the rsident page

--- solar-panel-planner/src/pages/ResidentPage.jsx
+++ solar-panel-planner/src/pages/ResidentPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
-
+import ResidentForm from "../ResidentForm"
 
 export default function ResidentPage() {
   return (
     <main className="w-full bg-background">
       <Navbar />
-      
+      <ResidentForm />
       </main>
   )
 }
