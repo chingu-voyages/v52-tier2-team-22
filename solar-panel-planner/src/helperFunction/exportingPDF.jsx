34365f3d3904fb37177b6615b263b396e762c545
Ayumi <ayuayuayuayus@gmail.com>
2024-12-12 00:57:08 +0100

    Create helper function for exporting PDF

--- solar-panel-planner/src/helperFunction/exportingPDF.jsx
+++ solar-panel-planner/src/helperFunction/exportingPDF.jsx
@@ -7,20 +7,28 @@ export const exportIndividualPDF = (record) => {
   doc.text(`Name: ${record.name}`, 10, 20);
   doc.text(`Email: ${record.email}`, 10, 30);
   doc.text(`Phone: ${record.phone}`, 10, 40);
-  doc.text(`Address: ${record.address.combinedAddress} ${record.address.zipcode}`, 10, 50);
-  doc.text(`Date: ${moment(record.requestDate).format("YYYY-MM-DD h:mm a")}`, 10, 60);
+  doc.text(
+    `Address: ${record.address.combinedAddress} ${record.address.zipcode}`,
+    10,
+    50
+  );
+  doc.text(
+    `Date: ${moment(record.requestDate).format("YYYY-MM-DD h:mm a")}`,
+    10,
+    60
+  );
   doc.save(`${record.name}_appointment.pdf`);
 };
 
 export const exportListPDF = (list, day) => {
   const doc = new jsPDF();
-  const pageHeight = doc.internal.pageSize.getHeight(); 
+  const pageHeight = doc.internal.pageSize.getHeight();
   const pageWidth = doc.internal.pageSize.getWidth();
   const lineHeight = 5;
   const margin = 10;
   const contentWidth = pageWidth - 2 * margin;
 
-  let y = 16; 
+  let y = 16;
   doc.setFontSize(14);
   doc.text(`Optimized Route Addresses for ${day}`, margin, y);
   y += 10;
@@ -30,25 +38,25 @@ export const exportListPDF = (list, day) => {
   y += 10;
 
   list.forEach((address, index) => {
-      if (index !== 0) {
-          const details = `${index + 1}. ${address.name}, 
+    if (index !== 0) {
+      const details = `${index + 1}. ${address.name}, 
           Time: ${moment(address.date).format("h:mm a")}, 
           Address: ${address.address}, 
           Phone: ${address.phone}, 
           Email: ${address.email}`;
-          const splitText = doc.splitTextToSize(details, contentWidth);
+      const splitText = doc.splitTextToSize(details, contentWidth);
 
-          splitText.forEach((line) => {
-              if (y + lineHeight > pageHeight - margin) {
-                  doc.addPage(); 
-                  y = margin;
-              }
-              doc.text(line, margin, y);
-              y += lineHeight;
-          });
+      splitText.forEach((line) => {
+        if (y + lineHeight > pageHeight - margin) {
+          doc.addPage();
+          y = margin;
+        }
+        doc.text(line, margin, y);
+        y += lineHeight;
+      });
 
-          y += 4;
-      }
+      y += 4;
+    }
   });
 
   doc.save("optimized_route.pdf");
