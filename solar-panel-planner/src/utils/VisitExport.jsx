99dab046a1b79cabcd02ecad469add32c355ff04
Ayumi <ayuayuayuayus@gmail.com>
2024-12-13 21:54:30 +0100

    Showing a date in PDF

--- solar-panel-planner/src/utils/VisitExport.jsx
+++ solar-panel-planner/src/utils/VisitExport.jsx
@@ -4,11 +4,10 @@ import { exportListPDF } from "./exportingPDF";
 import moment from "moment";
 
 export default function VisitList({ listOfDay, selectedDay, listOfToday }) {
-  // const [error, setError] = useState(null);
   const [isAlertVisible, setIsAlertVisible] = useState(false);
   const today = moment().format("YYYY-MM-DD");
 
-  const exportList = (selectedDay ? listOfDay : listOfToday)
+  const exportList = (selectedDay === today ? listOfToday : listOfDay)
     .filter((user) => user.status === "confirmed")
     .map((user) => ({
       coord: user.address.coord,
@@ -60,7 +59,7 @@ export default function VisitList({ listOfDay, selectedDay, listOfToday }) {
               exportList[exportList.length - 1], // Add the endpoint
             ];
             const orderedAddresses = ordered;
-            exportListPDF(orderedAddresses, (selectedDay = today));
+            exportListPDF(orderedAddresses, selectedDay);
           } else {
             alert("Failed to retrieve directions: " + status);
           }
@@ -68,50 +67,6 @@ export default function VisitList({ listOfDay, selectedDay, listOfToday }) {
       );
     };
     await getOptimizedRoute();
-    // if (exportList.length > 2) {
-    //   setIsAlertVisible(false);
-    //   const getOptimizedRoute = async () => {
-    //     const directionsService = new window.google.maps.DirectionsService();
-    //     const origin = startPoint.coord;
-    //     const destination = exportList[exportList.length - 1].coord;
-    //     const waypoints = exportList.slice(0, -1).map((address) => ({
-    //       location: `${address.coord.lat},${address.coord.lng}`,
-    //       stopover: true,
-    //     }));
-
-    //     await directionsService?.route(
-    //       {
-    //         origin: new window.google.maps.LatLng(origin.lat, origin.lng),
-    //         destination: new window.google.maps.LatLng(
-    //           destination.lat,
-    //           destination.lng
-    //         ),
-    //         waypoints: waypoints,
-    //         travelMode: window.google.maps.TravelMode.DRIVING,
-    //         optimizeWaypoints: true,
-    //       },
-    //       (result, status) => {
-    //         if (status === "OK") {
-    //           const optimizedOrder = result.routes[0].waypoint_order;
-
-    //           // Rearrange addresses based on the optimized order
-    //           const ordered = [
-    //             startPoint, // Add the starting point
-    //             ...optimizedOrder.map((index) => exportList[index]), // Adjust for slice(1, -1)
-    //             exportList[exportList.length - 1], // Add the endpoint
-    //           ];
-    //           const orderedAddresses = ordered;
-    //           exportListPDF(orderedAddresses, (selectedDay = today));
-    //         } else {
-    //           alert("Failed to retrieve directions: " + status);
-    //         }
-    //       }
-    //     );
-    //   };
-    //   await getOptimizedRoute();
-    // } else {
-    //   setIsAlertVisible(true);
-    // }
   };
 
   return (
@@ -120,7 +75,7 @@ export default function VisitList({ listOfDay, selectedDay, listOfToday }) {
         onClick={getRoute}
         className="bg-primaryGreen text-white px-4 py-2 rounded hover:bg-secondaryGreen"
       >
-        Export {selectedDay ? selectedDay : "today's"} route
+        Export {selectedDay === today ? "today's" : selectedDay} route
         <img src={DownloadIcon} className="h-5 pl-2 inline mb-1" />
       </button>
       {isAlertVisible && (
